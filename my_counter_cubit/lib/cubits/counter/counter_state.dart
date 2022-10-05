eedc18e826750fc440020eb9145217f423240d75
theDashpuntsag11 <thedashpuntsag@gmail.com>
2022-10-05 14:07:04 +0800

    adas;

---
+++ my_counter_cubit/lib/cubits/counter/counter_state.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'counter_cubit.dart';
+
+class CounterState extends Equatable {
+  final int counter;
+
+  const CounterState({
+    required this.counter,
+  });
+
+  factory CounterState.initial() {
+    return const CounterState(counter: 0);
+  }
+
+  @override
+  List<Object> get props => [counter];
+
+  @override
+  bool get stringify => true;
+
+  CounterState copyWith({
+    int? counter,
+  }) {
+    return CounterState(
+      counter: counter ?? this.counter,
+    );
+  }
+}
